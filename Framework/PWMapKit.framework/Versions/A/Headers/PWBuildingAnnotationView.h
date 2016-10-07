0a3dc16c7a7f60486a5dbf4aa543dc56d71fb07e
Xiangwei Wang <xwang@phunware.com>
2016-10-07 16:51:45 -0500

    Mapping SDK v3.0.1 release

---
+++ Framework/PWMapKit.framework/Versions/A/Headers/PWBuildingAnnotationView.h
@@ -0,0 +1,31 @@
+//
+//  PWBuildingAnnotationView.h
+//  PWMapKit
+//
+//  Copyright (c) 2015 Phunware. All rights reserved.
+//
+
+#import "PWPointOfInterest.h"
+
+@class PWAnnotationLabel;
+
+/**
+ The `PWAnnotationView` class is responsible for presenting visual building annotations in a map view. Annotation views are loosely coupled with a corresponding building annotation object that conforms to the `PWBuildingAnnotation` protocol. When an annotation’s coordinate is in the visible region, the map view asks its delegate to provide a corresponding annotation view. Annotation views may be recycled later and put into a reuse queue maintained by the map view.
+ */
+
+@interface PWBuildingAnnotationView : MKAnnotationView
+
+@property (nonatomic, weak) UIView *containerView;
+
+/**
+ The annotation label associated with the view. The annotation label stroke color and stroke width can be customized. 
+ 
+ @discussion The annotation label is automatically displayed or hidden based on whether or not it occludes other annotation view images or labels.
+ */
+@property (nonatomic, weak) PWAnnotationLabel *label;
+
+@property (nonatomic, weak) UIImageView *imageView;
+
+
+
+@end
