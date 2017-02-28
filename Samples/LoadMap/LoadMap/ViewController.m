72bb73d473d21831590bbf1752e2fe3748706038
Xiangwei Wang <xwang@phunware.com>
2017-02-28 10:34:29 -0600

    Update README.md to add privacy

--- Samples/LoadMap/LoadMap/ViewController.m
+++ Samples/LoadMap/LoadMap/ViewController.m
@@ -28,6 +28,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    CGSize size = self.loadMapButton.frame.size;
+    UIBezierPath *path = [self arrowFramePath:size];
+    
+    CAShapeLayer *shapeView = [[CAShapeLayer alloc] init];
+    [shapeView setPath:path.CGPath];
+    shapeView.fillColor = UIColor.whiteColor.CGColor;
+    shapeView.lineWidth = 1.0;
+    shapeView.strokeColor = UIColor.grayColor.CGColor;
+    [self.loadMapButton.layer addSublayer:shapeView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -57,6 +67,41 @@
             [mapViewController.mapView registerLocationManager:manager];
         }];
     }];
+}
+
+- (UIBezierPath *)arrowFramePath:(CGSize)size
+{
+    CGFloat radius = 5.0;
+    CGFloat arrowWidth = 5.0;
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    // top left
+    [path addArcWithCenter:CGPointMake(radius, radius) radius:radius startAngle:M_PI endAngle:3*M_PI_2 clockwise:YES];
+    
+    [path addLineToPoint:CGPointMake(size.width/2 - arrowWidth, 0)];
+    [path addLineToPoint:CGPointMake(size.width/2, -arrowWidth)];
+    [path addLineToPoint:CGPointMake(size.width/2 + arrowWidth, 0)];
+    
+    [path addArcWithCenter:CGPointMake(size.width - radius, radius) radius:radius startAngle:3*M_PI_2 endAngle:0 clockwise:YES];
+    
+    [path addLineToPoint:CGPointMake(size.width, size.height/2 - arrowWidth)];
+    [path addLineToPoint:CGPointMake(size.width + arrowWidth, size.height/2)];
+    [path addLineToPoint:CGPointMake(size.width, size.height/2 + arrowWidth)];
+    
+    [path addArcWithCenter:CGPointMake(size.width - radius, size.height - radius) radius:radius startAngle:0 endAngle:M_PI_2 clockwise:YES];
+    
+    [path addLineToPoint:CGPointMake(size.width/2 + arrowWidth, size.height)];
+    [path addLineToPoint:CGPointMake(size.width/2, size.height + arrowWidth)];
+    [path addLineToPoint:CGPointMake(size.width/2 - arrowWidth, size.height)];
+    
+    [path addArcWithCenter:CGPointMake(radius, size.height - radius) radius:radius startAngle:M_PI_2 endAngle:M_PI clockwise:YES];
+    
+    [path addLineToPoint:CGPointMake(0, size.height/2 + arrowWidth)];
+    [path addLineToPoint:CGPointMake(-arrowWidth, size.height/2)];
+    [path addLineToPoint:CGPointMake(0, size.height/2 - arrowWidth)];
+    
+    [path closePath];
+    
+    return path;
 }
 
 @end
