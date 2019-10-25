e75cd35c31390db65ae1659576d2f8a4cae9b83b
Aaron Pendley <apendley@phunware.com>
2019-10-25 10:24:31 -0500

    SWMAAS-1431 Simplify presentation logic in ArrivedManeuverViewModel

--- Samples/MapScenarios/MapScenarios/Maneuver/ArrivedManeuverViewModel.swift
+++ Samples/MapScenarios/MapScenarios/Maneuver/ArrivedManeuverViewModel.swift
@@ -26,18 +26,12 @@ struct ArrivedManeuverViewModel {
 
 extension ArrivedManeuverViewModel: ManeuverViewModel {
     var attributedText: NSAttributedString {
+        let templateString = NSLocalizedString("$0 at $1", comment: "$0 = 'You have arrived', $1 = destination name")
         let youHaveArrivedString = NSLocalizedString("You have arrived", comment: "")
-        let attributed: NSMutableAttributedString
         
-        if let destinationName = destinationName {
-            let templateString = NSLocalizedString("$0 at $1", comment: "$0 = 'You have arrived', $1 = destination name")
-            attributed = NSMutableAttributedString(string: templateString, attributes: standardOptions.attributes)
-            
-            attributed.replace(substring: "$0", with: youHaveArrivedString, attributes: highlightOptions.attributes)
-            attributed.replace(substring: "$1", with: destinationName, attributes: standardOptions.attributes)
-        } else {
-            attributed = NSMutableAttributedString(string: youHaveArrivedString, attributes: highlightOptions.attributes)
-        }
+        let attributed = NSMutableAttributedString(string: templateString, attributes: standardOptions.attributes)
+        attributed.replace(substring: "$0", with: youHaveArrivedString, attributes: highlightOptions.attributes)
+        attributed.replace(substring: "$1", with: destinationDisplayName, attributes: standardOptions.attributes)
         
         // Place a period at the end, using the same attributes as the last attributed substring.
         if let lastAttributes = attributed.lastAttributes {
@@ -50,5 +44,13 @@ extension ArrivedManeuverViewModel: ManeuverViewModel {
     
     var image: UIImage {
         return #imageLiteral(resourceName: "RouteListInstructionArrived")
+    }
+    
+    private var destinationDisplayName: String {
+        if let destinationName = destinationName {
+            return destinationName
+        } else {
+            return NSLocalizedString("your destination", comment: "")
+        }
     }
 }
