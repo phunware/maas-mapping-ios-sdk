00599812832aaaa0800897ee3792c76cf32ddab2
Aaron Pendley <apendley@phunware.com>
2019-10-30 17:05:23 -0500

    Use system colors to better support dark mode

--- Samples/MapScenarios/MapScenarios/Extensions/UIColor+Helper.swift
+++ Samples/MapScenarios/MapScenarios/Extensions/UIColor+Helper.swift
@@ -10,10 +10,6 @@ import UIKit
 
 extension UIColor {
     
-    class var nasa: UIColor {
-        return UIColor(displayP3Red: 4.0/255.0, green: 114.0/255.0, blue: 254.0/255.0, alpha: 1.0)
-    }
-    
     class var darkerGray: UIColor {
         return UIColor(displayP3Red: 47.0/255.0, green: 47.0/255.0, blue: 47.0/255.0, alpha: 1.0)
     }
