c86eb3bc0226d4f65840e37c3992b9f846c8e14e
jenkins <hostmaster@phunware.com>
2020-01-27 11:27:26 -0800

    Updating PWMapKit to v3.9.1. See CHANGELOG for details.

--- Framework/PWMapKit.framework/Headers/PWMapKit.h
+++ Framework/PWMapKit.framework/Headers/PWMapKit.h
@@ -28,7 +28,7 @@
 #import <PWMapKit/PWSharedLocation.h>
 #import <PWMapKit/PWLocationSharingDelegate.h>
 
-static NSString *const PWMapKitVersion = @"3.9.0";
+static NSString *const PWMapKitVersion = @"3.9.1";
 
 @interface PWMapKit : NSObject
 
